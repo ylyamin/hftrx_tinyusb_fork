8e003317ade0d6a644eb4853a8d431d069b3e7ea
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-11-20 18:12:50 +0300

    bth host: enumertion complete call

--- src/class/bth/bth_host.c
+++ src/class/bth/bth_host.c
@@ -17,6 +17,41 @@
 #define TU_LOG_DRV(...)   TU_LOG(CFG_TUH_BTH_LOG_LEVEL, __VA_ARGS__)
 
 
+static enum {
+    USBH_OUT_OFF,
+    USBH_OUT_IDLE,
+    USBH_OUT_CMD,
+    USBH_OUT_ACL_SEND,
+    USBH_OUT_ACL_POLL,
+} usbh_out_state;
+
+static enum {
+    USBH_IN_OFF,
+    USBH_IN_SUBMIT_REQUEST,
+    USBH_IN_POLL,
+} usbh_in_state;
+
+// higher-layer callbacks
+static void (*usbh_packet_sent)(void);
+static void (*usbh_packet_received)(uint8_t packet_type, uint8_t * packet, uint16_t size);
+
+// class state
+
+// outgoing
+static const uint8_t * cmd_packet;
+static uint16_t        cmd_len;
+
+static const uint8_t * acl_packet;
+static uint16_t        acl_len;
+
+// incoming
+static uint16_t hci_event_offset;
+static uint8_t hci_event[258];
+
+static uint16_t hci_acl_in_offset;
+//static uint8_t  hci_acl_in_buffer[HCI_INCOMING_PRE_BUFFER_SIZE + HCI_ACL_BUFFER_SIZE];
+//static uint8_t  * hci_acl_in_packet = &hci_acl_in_buffer[HCI_INCOMING_PRE_BUFFER_SIZE];
+
  //--------------------------------------------------------------------+
  // Host BTH Interface
  //--------------------------------------------------------------------+
@@ -90,9 +125,46 @@ static inline bthh_interface_t* get_itf(uint8_t idx)
 }
 
 
+uint8_t tuh_bth_itf_get_index(uint8_t daddr, uint8_t itf_num)
+{
+  for(uint8_t i=0; i<CFG_TUH_CDC; i++)
+  {
+    const bthh_interface_t* p_bth = &bthh_data[i];
+
+    if (p_bth->daddr == daddr && p_bth->bInterfaceNumber == itf_num) return i;
+  }
+
+  return TUSB_INDEX_INVALID_8;
+}
+
 bool bthh_set_config(uint8_t dev_addr, uint8_t itf_num)
 {
-	return false;
+	PRINTF("bthh_set_config: dev_addr=%u,itf_num=%u\n",dev_addr, itf_num);
+	  tusb_control_request_t request = { 0 };
+	  request.wIndex = tu_htole16((uint16_t) itf_num);
+
+	  // fake transfer to kick-off process
+	  tuh_xfer_t xfer;
+	  xfer.daddr  = dev_addr;
+	  xfer.result = XFER_RESULT_SUCCESS;
+	  xfer.setup  = &request;
+	  xfer.user_data = 0; // initial state
+
+	  uint8_t const idx = tuh_bth_itf_get_index(dev_addr, itf_num);
+	  bthh_interface_t * p_bth = get_itf(idx);
+	  //TU_ASSERT(p_bth && p_bth->serial_drid < SERIAL_DRIVER_COUNT);
+
+	  //serial_drivers[p_bth->serial_drid].process_set_config(&xfer);
+	  TP();
+	  // Prepare for incoming data
+	  //tu_edpt_stream_read_xfer(&p_bth->stream.acl_in);
+	  TP();
+
+	  // notify usbh that driver enumeration is complete
+	  usbh_driver_set_config_complete(p_bth->daddr, itf_num);
+	  TP();
+
+	  return true;
 }
 
 bool bthh_xfer_cb(uint8_t dev_addr, uint8_t ep_addr, xfer_result_t event, uint32_t xferred_bytes)
@@ -103,6 +175,7 @@ bool bthh_xfer_cb(uint8_t dev_addr, uint8_t ep_addr, xfer_result_t event, uint32
 void bthh_init(void)
 {
 	tu_memclr(bthh_data, sizeof(bthh_data));
+	PRINTF("bthh_init:\n");
 
 	for (size_t i=0; i<CFG_TUH_BTH; i++)
 	{
@@ -159,6 +232,22 @@ bool bthh_open(uint8_t rhport, uint8_t dev_addr, tusb_desc_interface_t const *it
 		  }
 
 		  p_bth->itf_num = itf_desc->bInterfaceNumber;
+
+
+		  //++++++++++++++
+
+
+		    // Command
+		    usbh_out_state = USBH_OUT_OFF;
+
+		    // Event In
+		    usbh_in_state = USBH_IN_OFF;
+
+		    // ACL In
+		    hci_acl_in_offset = 0;
+
+		    // ACL Out
+		  //--------------
 
 		  return true;
 	  }
