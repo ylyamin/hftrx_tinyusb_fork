de0440fecd347a317b46d1871a336883c6a8cbbe
Genadi V. Zawidowski <mgs2001@mail.ru>
2023-11-20 19:11:51 +0300

    save state

--- src/class/bth/bth_host.c
+++ src/class/bth/bth_host.c
@@ -76,8 +76,6 @@ typedef struct {
 	uint8_t itf_num;
 	//tuh_xfer_cb_t user_control_cb;
 	uint8_t ep_notif;
-	uint8_t acl_in;
-	uint8_t acl_out;
 
 	struct {
 	 tu_edpt_stream_t acl_in;
@@ -100,7 +98,7 @@ static bthh_interface_t* make_new_itf(uint8_t daddr, tusb_desc_interface_t const
   for(uint8_t i=0; i<CFG_TUH_CDC; i++)
   {
     if (bthh_data[i].daddr == 0) {
-      bthh_interface_t* p_bth = &bthh_data[i];
+      bthh_interface_t* const p_bth = &bthh_data[i];
 
       p_bth->daddr              = daddr;
       p_bth->bInterfaceNumber   = itf_desc->bInterfaceNumber;
@@ -115,18 +113,29 @@ static bthh_interface_t* make_new_itf(uint8_t daddr, tusb_desc_interface_t const
 }
 
 
-static inline bthh_interface_t* get_itf(uint8_t idx)
-{
-	PRINTF("bt get_itf idx=%u\n", idx);
+static inline bthh_interface_t* get_itf(uint8_t idx) {
+//PRINTF("bt get_itf idx=%u\n", idx);
   TU_ASSERT(idx < CFG_TUH_BTH, NULL);
   bthh_interface_t* p_bth = &bthh_data[idx];
 
   return (p_bth->daddr != 0) ? p_bth : NULL;
 }
 
+static inline uint8_t get_idx_by_ep_addr(uint8_t daddr, uint8_t ep_addr) {
+  for(uint8_t i=0; i<CFG_TUH_CDC; i++)
+  {
+    bthh_interface_t* p_bth = &bthh_data[i];
+    if ( (p_bth->daddr == daddr) &&
+         (ep_addr == p_bth->ep_notif || ep_addr == p_bth->stream.acl_in.ep_addr || ep_addr == p_bth->stream.acl_out.ep_addr))
+    {
+      return i;
+    }
+  }
 
-uint8_t tuh_bth_itf_get_index(uint8_t daddr, uint8_t itf_num)
-{
+  return TUSB_INDEX_INVALID_8;
+}
+
+uint8_t tuh_bth_itf_get_index(uint8_t daddr, uint8_t itf_num) {
   for(uint8_t i=0; i<CFG_TUH_CDC; i++)
   {
     const bthh_interface_t* p_bth = &bthh_data[i];
@@ -139,43 +148,71 @@ uint8_t tuh_bth_itf_get_index(uint8_t daddr, uint8_t itf_num)
 
 bool bthh_set_config(uint8_t dev_addr, uint8_t itf_num)
 {
-	PRINTF("bthh_set_config: dev_addr=%u,itf_num=%u\n",dev_addr, itf_num);
-	  tusb_control_request_t request = { 0 };
-	  request.wIndex = tu_htole16((uint16_t) itf_num);
-
-	  // fake transfer to kick-off process
-	  tuh_xfer_t xfer;
-	  xfer.daddr  = dev_addr;
-	  xfer.result = XFER_RESULT_SUCCESS;
-	  xfer.setup  = &request;
-	  xfer.user_data = 0; // initial state
-
+	PRINTF("bthh_set_config: itf_num=%u\n", itf_num);
+//	  tusb_control_request_t request = { 0 };
+//	  request.wIndex = tu_htole16((uint16_t) itf_num);
+//
+//	  // fake transfer to kick-off process
+//	  tuh_xfer_t xfer;
+//	  xfer.daddr  = dev_addr;
+//	  xfer.result = XFER_RESULT_SUCCESS;
+//	  xfer.setup  = &request;
+//	  xfer.user_data = 0; // initial state
+//
+//
+//
 	  uint8_t const idx = tuh_bth_itf_get_index(dev_addr, itf_num);
-	  bthh_interface_t * p_bth = get_itf(idx);
-	  //TU_ASSERT(p_bth && p_bth->serial_drid < SERIAL_DRIVER_COUNT);
+	  bthh_interface_t * const p_bth = get_itf(idx);
+	PRINTF("bthh_set_config: idx=%u\n", idx);
 
-	  //serial_drivers[p_bth->serial_drid].process_set_config(&xfer);
-	  TP();
 	  // Prepare for incoming data
-	  //tu_edpt_stream_read_xfer(&p_bth->stream.acl_in);
-	  TP();
+	  tu_edpt_stream_read_xfer(&p_bth->stream.acl_in);
 
 	  // notify usbh that driver enumeration is complete
 	  usbh_driver_set_config_complete(p_bth->daddr, itf_num);
-	  TP();
 
 	  return true;
 }
 
-bool bthh_xfer_cb(uint8_t dev_addr, uint8_t ep_addr, xfer_result_t event, uint32_t xferred_bytes)
-{
-	return false;
+bool bthh_xfer_cb(uint8_t dev_addr, uint8_t ep_addr, xfer_result_t event, uint32_t xferred_bytes) {
+	  // TODO handle stall response, retry failed transfer ...
+	  TU_ASSERT(event == XFER_RESULT_SUCCESS);
+	  PRINTF("bthh_xfer_cb: dev_addr=%u, ep_addr=%u, xferred_bytes=%u\n", dev_addr, ep_addr, xferred_bytes);
+
+	  uint8_t const idx = get_idx_by_ep_addr(dev_addr, ep_addr);
+	  bthh_interface_t * p_bth = get_itf(idx);
+	  TU_ASSERT(p_bth);
+
+	  if ( ep_addr == p_bth->stream.acl_out.ep_addr ) {
+	    // invoke tx complete callback to possibly refill tx fifo
+	    ////if (tuh_bth_tx_complete_cb) tuh_bth_tx_complete_cb(idx);
+
+	    if ( 0 == tu_edpt_stream_write_xfer(&p_bth->stream.acl_out) ) {
+	      // If there is no data left, a ZLP should be sent if:
+	      // - xferred_bytes is multiple of EP Packet size and not zero
+	      tu_edpt_stream_write_zlp_if_needed(&p_bth->stream.acl_out, xferred_bytes);
+	    }
+	  }
+	  else if ( ep_addr == p_bth->stream.acl_in.ep_addr ) {
+	      tu_edpt_stream_read_xfer_complete(&p_bth->stream.acl_in, xferred_bytes);
+
+	    // invoke receive callback
+	    ////if (tuh_bth_rx_cb) tuh_bth_rx_cb(idx);
+
+	    // prepare for next transfer if needed
+	    tu_edpt_stream_read_xfer(&p_bth->stream.acl_in);
+	  }else if ( ep_addr == p_bth->ep_notif ) {
+	    // TODO handle notification endpoint
+	  }else {
+	    TU_ASSERT(false);
+	  }
+
+	  return true;
 }
 
 void bthh_init(void)
 {
 	tu_memclr(bthh_data, sizeof(bthh_data));
-	PRINTF("bthh_init:\n");
 
 	for (size_t i=0; i<CFG_TUH_BTH; i++)
 	{
@@ -205,8 +242,6 @@ bool bthh_open(uint8_t rhport, uint8_t dev_addr, tusb_desc_interface_t const *it
 		  3 == itf_desc->bNumEndpoints
 		  )
 	  {
-		  PRINTF("BT RADIO found!\n");
-
 		  bthh_interface_t * p_bth = make_new_itf(dev_addr, itf_desc);
 		  TU_VERIFY(p_bth);
 		  if (p_bth == NULL)
@@ -221,17 +256,20 @@ bool bthh_open(uint8_t rhport, uint8_t dev_addr, tusb_desc_interface_t const *it
 				TU_ASSERT(tuh_edpt_open(dev_addr, ep_desc));
 			}
 			else if (TUSB_DIR_IN == tu_edpt_dir(ep_desc->bEndpointAddress)) {
-				p_bth->acl_in = ep_desc->bEndpointAddress;
+				p_bth->stream.acl_in.ep_addr = ep_desc->bEndpointAddress;
 				TU_ASSERT(tuh_edpt_open(dev_addr, ep_desc));
+			      tu_edpt_stream_open(&p_bth->stream.acl_in, p_bth->daddr, ep_desc);
 			} else {
-				p_bth->acl_out = ep_desc->bEndpointAddress;
+				p_bth->stream.acl_out.ep_addr = ep_desc->bEndpointAddress;
 				TU_ASSERT(tuh_edpt_open(dev_addr, ep_desc));
+			      tu_edpt_stream_open(&p_bth->stream.acl_out, p_bth->daddr, ep_desc);
 			}
 
 		    ep_desc = (tusb_desc_endpoint_t const*) tu_desc_next(ep_desc);
 		  }
 
 		  p_bth->itf_num = itf_desc->bInterfaceNumber;
+			PRINTF("bthh_open: itf_num=%u\n", p_bth->itf_num);
 
 
 		  //++++++++++++++
